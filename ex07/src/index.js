289301ea26e0b236153b8aa7240048981d3e85e2
Dami-art <damjana.zoric@gmail.com>
2021-03-13 09:49:04 +0100

    all

---
+++ ex07/src/index.js
@@ -0,0 +1,13 @@
+function main (){
+    //Only change code below this line
+    var someAddjective = "the best!";
+    var myString = "Mistral is ";
+    myString += someAddjective;
+    //Only change code above this line
+
+     return myString;
+
+    }
+    
+    console.log(main());
+    module.exports = main;
---
+++ ex13/src/index.js
@@ -0,0 +1,12 @@
+function main (){
+    //Only change code below this line
+    var lastName = "Bezos";
+    var thirdtoLastLetterOfLastName = lastName[lastName.length-3];
+    //Only change code above this line
+
+     return thirdtoLastLetterOfLastName;
+
+    }
+    
+    console.log(main());
+    module.exports = main;
